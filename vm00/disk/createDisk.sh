abe7881b5eb4a6c77108f627fb7f7f50ab400705
Matthew Hassel <matthew.hassel@gmail.com>
2017-05-10 16:49:33 -0500

    making several changes   renaming OVMF to ovmf (dunno why i would ever capitalize anything)   adding iso link files to keep record of which iso was used where

--- vm00/disk/createDisk.sh
+++ vm00/disk/createDisk.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-dd if=/dev/zero of=./vm00.img bs=1M seek=20000 count=0
+#dd if=/dev/zero of=./vm00.img bs=1M seek=20000 count=0
+
+# the process is now to create a logical volume to use as the vms disk
+# Example below:
+#   lvcreate -n <nameOfVM> -L <size> <volume group>
+#   lvcreate -n win00.internal.matthassel.com -L 100G vm00
+
+#ln -s /dev/mapper/<volume group/<nameOfVM> .
